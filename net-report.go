5f67093575471d5980a3c7ad96479dab7c4d3887
dan <dan@hello-world.sh>
2020-04-23 13:07:55 +0200

    Update net-report.go

--- net-report.go
+++ net-report.go
@@ -82,7 +82,7 @@ func report(hostName string, topicName string) (err error){
 }
 
 func getOutboundIP() (IP net.IP, err error){
-    conn, err := net.Dial("udp", "8.8.8.8:80")
+    conn, err := net.Dial("udp", "8.8.8.8:53")
     if err != nil {
         return nil, err
     }
